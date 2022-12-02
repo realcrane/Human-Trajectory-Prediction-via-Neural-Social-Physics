df4a2a25bdebd0ba4c332cbb3f99a94608d63c0a
JiangbeiYue <108633019+JiangbeiYue@users.noreply.github.com>
2022-12-02 13:11:16 +0000

    Update readme.py

--- data/SDD/test_pickle/readme.py
+++ data/SDD/test_pickle/readme.py
@@ -1,3 +1,2 @@
 Complete data can be found by the following link
-https://leeds365-my.sharepoint.com/:f:/g/personal/scjy_leeds_ac_uk/EmW3-ODjnfhPi-67OV9JTmoBnUjeVILxWncF-_3Pr1jPqg?e=SnkhpC
-    
+https://leeds365-my.sharepoint.com/:f:/g/personal/scjy_leeds_ac_uk/EnxDEQvyRpNIoi2BmoImP-QBnN4OCQMkGd3H6_4OH6-mzw?e=7LaKdt
